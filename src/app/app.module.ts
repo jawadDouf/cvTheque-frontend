17112be3e288299bcee9293f3c3acb4814c4c42e
jawadDouf <jdoufare2001@gmail.com>
2023-02-06 16:51:46 +0100

    update the search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AuthInterceptor } from 'src/app/interceptors/auth.interceptor';
 import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
-import { FooterComponent } from './shared/footer/footer.component';
-import { CmeViewApprenantsComponent } from './views/cme-view-apprenants/cme-view-apprenants.component';
+
 import { CorsInterceptorInterceptor } from './interceptors/cors-interceptor.interceptor';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { ApprenantCardComponent } from './components/apprenants/apprenant-card/apprenant-card.component';
+
 import { FormsModule } from '@angular/forms';
 
 import { CvComponent } from './components/apprenants/cv/cv.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CmeViewApprenantsComponent } from './views/cme-view-apprenants/cme-view-apprenants.component';
+import { ApprenantCardComponent } from './components/apprenants/apprenant-card/apprenant-card.component';
 //Set up the router
 const appRouter: Routes = [
   {path:'apprenants',component:CmeViewApprenantsComponent},
